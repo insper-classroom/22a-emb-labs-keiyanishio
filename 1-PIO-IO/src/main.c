91966630a2c060cc0db94c5164fe3024cf794832
keiyanishio <62714338+keiyanishio@users.noreply.github.com>
2022-02-22 20:50:03 -0300

    Update main.c

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,10 +23,33 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED_PIO            PIOC      
+#define LED_PIO_ID         ID_PIOC
+#define LED_PIO_IDX        8
+#define LED_PIO_IDX_MASK   (1 << LED_PIO_IDX)
+
+// Configuracoes do botao
+#define BUT_PIO        PIOA
+#define BUT_PIO_ID     ID_PIOA
+#define BUT_PIO_IDX    11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX) // esse já está pronto.
+
+//OLED
+#define OLED_PIO1       PIOA
+#define OLED_PIO1_ID    ID_PIOA
+#define OLED_PIO1_IDX   0
+#define OLED_PIO1_IDX_MASK (1 << OLED_PIO1_IDX)
+
+#define OLED_PIO2       PIOC
+#define OLED_PIO2_ID    ID_PIOC
+#define OLED_PIO2_IDX   30
+#define OLED_PIO2_IDX_MASK (1 << OLED_PIO2_IDX)
+
+#define OLED_PIO3       PIOB
+#define OLED_PIO3_ID    ID_PIOB
+#define OLED_PIO3_IDX   2
+#define OLED_PIO3_IDX_MASK (1 << OLED_PIO3_IDX)
+
 
 /************************************************************************/
 /* constants                                                            */
@@ -53,6 +76,37 @@ void init(void);
 // Função de inicialização do uC
 void init(void)
 {
+// Initialize the board clock
+	sysclk_init();
+
+// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;	
+	// Ativa o PIO na qual o LED foi conectado
+	// para que possamos controlar o LED e OLED.
+	pmc_enable_periph_clk(LED_PIO_ID);
+	
+	pmc_enable_periph_clk(OLED_PIO1_ID);
+	pmc_enable_periph_clk(OLED_PIO2_ID);
+	pmc_enable_periph_clk(OLED_PIO3_ID);
+	
+	//Inicializa PC8 e OLEDS como saída
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	
+	pio_set_output(OLED_PIO1, OLED_PIO1_IDX_MASK, 0, 0, 0);
+	pio_set_output(OLED_PIO2, OLED_PIO2_IDX_MASK, 0, 0, 0);
+	pio_set_output(OLED_PIO3, OLED_PIO3_IDX_MASK, 0, 0, 0);
+	
+	
+	// Inicializa PIO do botao
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	
+	// configura pino ligado ao botão como entrada com um pull-up.
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
+
+
+
+
 
 }
 
@@ -61,15 +115,29 @@ void init(void)
 /************************************************************************/
 
 // Funcao principal chamada na inicalizacao do uC.
-int main(void)
-{
-  init();
+// Funcao principal chamada na inicalizacao do uC.
+int main(void) {
+	// inicializa sistema e IOs
+	init();
 
-  // super loop
-  // aplicacoes embarcadas não devem sair do while(1).
-  while (1)
-  {
-
-  }
-  return 0;
+	// super loop
+	// aplicacoes embarcadas não devem sair do while(1).
+	while (1)
+	{
+		if (pio_get(PIOA, PIO_INPUT, BUT_PIO_IDX_MASK)!=1){
+			int i = 0;
+			while (i < 5){
+				pio_set(PIOC, LED_PIO_IDX_MASK);      // Coloca 1 no pino LED
+				delay_ms(200);                        // Delay por software de 200 ms
+				pio_clear(PIOC, LED_PIO_IDX_MASK);    // Coloca 0 no pino do LED
+				delay_ms(200); 		
+				i++;		
+			}
+		}
+		                      // Delay por software de 200 ms
+	}
+	return 0;
 }
+
+		 
+
