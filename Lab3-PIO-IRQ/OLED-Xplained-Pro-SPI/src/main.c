b778f1de0bef9e8f0707cdfeaceee84af6cb8827
keiyanishio <keiyan@al.insper.edu.br>
2022-03-10 14:13:18 -0300

    C feito

--- Lab3-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
+++ Lab3-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
@@ -9,10 +9,12 @@
 #define OLED_PIO1_IDX   0
 #define OLED_PIO1_IDX_MASK (1 << OLED_PIO1_IDX)
 
-#define BUT_PIO1       PIOD
-#define BUT_PIO1_ID    ID_PIOD
-#define BUT_PIO1_IDX   28
-#define BUT_PIO1_IDX_MASK (1u << BUT_PIO1_IDX)
+#define BUT1_PIO	      PIOD
+#define BUT1_PIO_ID    ID_PIOD
+#define BUT1_PIO_IDX   28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+
 
 
 // LED
@@ -29,16 +31,24 @@
 
 void io_init(void);
 void pisca_led(int n, int t);
-volatile char but_flag;
-volatile char but_flag1_press;
-volatile char but_flag1_up;
+
+volatile char but_flag = 0;
+volatile char but_flag1_press = 0;
+volatile char but_flag1_up = 0;
+
 
 void but_callback (void) {
-	but_flag =1;
+	if(but_flag == 1) 
+	{
+		but_flag = 0;
+}
+	else if(but_flag == 0){
+		 but_flag = 1;
+	}
 }
 
 void but_callback1 (void) {
-	if (pio_get(BUT_PIO1, PIO_INPUT, BUT_PIO1_IDX_MASK)) {
+	if (pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)) {
 		but_flag1_press = 0;
 		but_flag1_up = 1;
 		
@@ -49,6 +59,9 @@ void but_callback1 (void) {
 
 void pisca_led(int n, int t){
 	for (int i=0;i<n;i++){
+		if(but_flag1_press == 1){
+			break;
+		}
 		pio_clear(LED_PIO, LED_IDX_MASK);
 		delay_ms(t);
 		pio_set(LED_PIO, LED_IDX_MASK);
@@ -56,16 +69,16 @@ void pisca_led(int n, int t){
 	}
 }
 
-void display(int delay){
-	int periodo = 2*delay*0.001;
+void display(double delay){
+	double periodo = 2*delay*0.001;
 	double freq = 1/periodo;
-	char fstr[128];
+	char fstr[5];
 
 
-	sprintf(fstr, "%f", freq); //
+	sprintf(fstr, "%.4lf", freq); //
 	gfx_mono_draw_string("freq:", 0, 0, &sysfont);
 
-	gfx_mono_draw_string(fstr, 10, 0, &sysfont);
+	gfx_mono_draw_string(fstr, 60, 0, &sysfont);
 }
 
 	
@@ -79,28 +92,28 @@ void io_init(void)
 	pmc_enable_periph_clk(LED_PIO_ID);
 	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
 	
-	pmc_enable_periph_clk(OLED_PIO1);
+	pmc_enable_periph_clk(OLED_PIO1_ID);
 	pio_configure(OLED_PIO1, PIO_OUTPUT_0, OLED_PIO1_IDX_MASK, PIO_DEFAULT);
 
 	// Inicializa clock do periférico PIO responsavel pelo botao
 	pmc_enable_periph_clk(BUT_PIO_ID);
 	
-	pmc_enable_periph_clk(BUT_PIO1_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
 
 	// Configura PIO para lidar com o pino do botão como entrada
 	// com pull-up
 	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
 	pio_set_debounce_filter(BUT_PIO, BUT_IDX_MASK, 60);
 	
-	pio_configure(BUT_PIO1, PIO_INPUT, BUT_PIO1_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
-	pio_set_debounce_filter(BUT_PIO1, BUT_PIO1_IDX_MASK, 60);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT1_PIO, BUT1_PIO_IDX_MASK, 60);
 
 	// Configura interrupção no pino referente ao botao e associa
 	// função de callback caso uma interrupção for gerada
 	// a função de callback é a: but_callback()
-	pio_handler_set(BUT_PIO1,
-					BUT_PIO1_ID,
-					BUT_PIO1_IDX_MASK,
+	pio_handler_set(BUT1_PIO,
+					BUT1_PIO_ID,
+					BUT1_PIO_IDX_MASK,
 					PIO_IT_EDGE,
 					but_callback1);
 					
@@ -111,8 +124,8 @@ void io_init(void)
 					but_callback);
 
 	// Ativa interrupção e limpa primeira IRQ gerada na ativacao
-	pio_enable_interrupt(BUT_PIO1, BUT_PIO1_IDX_MASK);
-	pio_get_interrupt_status(BUT_PIO1);
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUT1_PIO);
 	
 	pio_enable_interrupt(BUT_PIO, BUT_IDX_MASK);
 	pio_get_interrupt_status(BUT_PIO);
@@ -120,8 +133,8 @@ void io_init(void)
 	
 	// Configura NVIC para receber interrupcoes do PIO do botao
 	// com prioridade 4 (quanto mais próximo de 0 maior)
-	NVIC_EnableIRQ(BUT_PIO1_ID);
-	NVIC_SetPriority(BUT_PIO1_ID, 4); // Prioridade 4
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 4); // Prioridade 4
 	
 	NVIC_EnableIRQ(BUT_PIO_ID);
 	NVIC_SetPriority(BUT_PIO_ID, 4);
@@ -133,8 +146,8 @@ int main (void)
 	sysclk_init();
 	io_init();
 	delay_init();
-	int delay = 200;
-	int cont = 0;
+	double delay = 200;
+	double cont = 0;
 	
 	
 	
@@ -149,17 +162,23 @@ int main (void)
 
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
+		
+		if (but_flag == 1){
+			pisca_led(30, delay);
+		}
+		
+		
+		
 		while (but_flag1_press){
 			cont++;
 		}
 		if (but_flag1_up){
-			if (cont>30000){
-				delay-=100;
+			if (cont>3000000){
+				delay+=100;
 				display(delay);
 			}
-			else{
-				delay+=100;
-				
+			else if (cont<3000000 && delay>100){
+				delay-=100;
 				display(delay);
 			}
 			
@@ -167,9 +186,7 @@ int main (void)
 			cont=0;	
 			
 		}
-		if (but_flag){
-			pisca_led(30, delay);
-		}
+		
 		
 		
 		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
